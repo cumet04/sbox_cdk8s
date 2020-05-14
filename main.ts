6c4e9b4bd056079506096ed33ed21ee3c0da5523
cumet04 <medalhkr@gmail.com>
2020-05-14 16:24:35 +0900

    getting started 2

--- main.ts
+++ main.ts
@@ -1,42 +1,16 @@
 import { Construct } from "constructs";
 import { App, Chart } from "cdk8s";
-
-import { Deployment, Service, IntOrString } from "./imports/k8s";
+import { WebService } from "./lib/web-service";
 
 class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    const label = { app: "hello-k8s" };
-
-    new Service(this, "service", {
-      spec: {
-        type: "LoadBalancer",
-        ports: [{ port: 80, targetPort: IntOrString.fromNumber(8080) }],
-        selector: label,
-      },
+    new WebService(this, "hello", {
+      image: "paulbouwer/hello-kubernetes:1.7",
+      replicas: 2,
     });
-
-    new Deployment(this, "deployment", {
-      spec: {
-        replicas: 2,
-        selector: {
-          matchLabels: label,
-        },
-        template: {
-          metadata: { labels: label },
-          spec: {
-            containers: [
-              {
-                name: "hello-kubernetes",
-                image: "paulbouwer/hello-kubernetes:1.7",
-                ports: [{ containerPort: 8080 }],
-              },
-            ],
-          },
-        },
-      },
-    });
+    new WebService(this, "ghost", { image: "ghost", containerPort: 2368 });
   }
 }
 
